314df3955963648ea52c6ff32826d646d7c9ae57
HappySmile <153109864+devmad001@users.noreply.github.com>
2024-01-04 15:47:19 -0600

    q2

--- datasets.py
+++ datasets.py
@@ -42,6 +42,11 @@ def rocstories(data_dir, n_train=1497, n_valid=374):
         trX2.append(c1)
         trX3.append(c2)
         trY.append(y)
+    for s, c1, c2, y in zip(tr_storys, tr_comps1, tr_comps2, tr_ys):
+        trX1.append(s)
+        trX2.append(c1)
+        trX3.append(c2)
+        trY.append(y)
 
     vaX1, vaX2, vaX3 = [], [], []
     vaY = []
